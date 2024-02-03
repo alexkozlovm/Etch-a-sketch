870376667384ed39811226d5aabfe9dde9c8ffb6
Alexander Kozlov <alex-k@akozlov.lan>
2024-02-03 10:34:22 -0500

    added pen, eraser, and rainbow functionality

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 // Get all the .cell divs
 const cells = document.querySelectorAll('.cell');
 let color = 'white';
+let mode = 'pen';
+
+function active () {
+    if (mode === 'pen') {
+        color = 'white';
+    } else if (mode === 'eraser') {
+        color = '#282c34';
+    } else {
+        color = `#${Math.floor(Math.random()*16777215).toString(16)}`;
+    }
+}
 
 // Add event listener to each cell
 cells.forEach(cell => {
     cell.addEventListener('mouseover', () => {
         if (event.buttons === 1) {
+            active()
             cell.style.backgroundColor = color;
             cell.style.cursor = 'crosshair';
         }
